9a7eb9b8087194e3d5135acb5403d257c63fafcc
Milind <milindsharmagithub@gmail.com>
2023-07-03 04:21:59 +0530

    Project Complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import RootLayout from "./Layouts/RootLayout";
 import HelpLayout from "./Layouts/HelpLayout";
-import Contact from "./help/Contact";
+import Contact, { contactAction } from "./help/Contact";
 import Faq from "./help/Faq";
 import NotFound from "./pages/NotFound";
 import Careers, { careersLoader } from "./Layouts/Careers";
@@ -23,7 +23,7 @@ const router = createBrowserRouter(
       <Route path="about" element={<About/>}/>
       <Route path="help" element={<HelpLayout/>}>
         <Route path="Faq" element={<Faq/>}/>
-        <Route path="contact" element={<Contact/>}/>
+        <Route path="contact" element={<Contact/>} action={contactAction}/>
       </Route>
 
       <Route path="careers" element={<CareersLayout/>} errorElement={<CareerError/>}>
